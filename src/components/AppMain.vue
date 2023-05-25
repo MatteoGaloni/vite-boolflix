9333afddec0d4b669025e60e52fc051ef7044a72
MatteoGaloni <mtgaloni@gmail.com>
2023-05-25 16:33:35 +0200

    add user movies search

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,5 +1,6 @@
 <script>
-
+import { store } from '../data/store';
+import axios from 'axios';
 
 export default {
     name: "AppMain",
@@ -7,15 +8,35 @@ export default {
     data() {
         return {
             store,
+            userSearch: "Ricerca"
         }
     },
-    methods: {},
+    methods: {
+        searchMovie() {
+            this.store.genericMovieUrl = "https://api.themoviedb.org/3/search/movie?api_key=88d5ff9ebc9aa6659450ea8f3fdc7586&query=",
+                this.store.genericMovieUrl += this.userSearch
+            console.log("ora l'url è", this.store.genericMovieUrl)
+            axios.get(this.store.genericMovieUrl).then(r => {
+                this.store.selectedMovies = r.data.results;
+                console.log(this.store.selectedMovies)
+                store.loading = true
+            })
+        }
+    }
+
 
 }
 </script>
 
 <template>
-    <h2>IO SONO IL MAIN</h2>
+    <input v-model="userSearch" type="text">
+    <button @click="searchMovie()">CLICCAMI</button>
+    <div v-for="movie in store.selectedMovies">
+        <li>TITOLO =-------------------------- {{ movie.title }}</li>
+        <li>TITOLO ORIGINALE =---------------- {{ movie.original_title }}</li>
+        <li>LINGUA ORIGINALE =---------------- {{ movie.original_language }}</li>
+        <li>VOTO =---------------------------- {{ movie.popularity }}</li>
+    </div>
 </template>
 
 <style scoped lang="scss">
--- src/data/store.js
+++ src/data/store.js
@@ -4,4 +4,6 @@ export const store = reactive({
 
     prova: "prova",
     apiKey: "88d5ff9ebc9aa6659450ea8f3fdc7586",
+    genericMovieUrl: "https://api.themoviedb.org/3/search/movie?api_key=88d5ff9ebc9aa6659450ea8f3fdc7586&query=",
+    selectedMovies:[],
 })
